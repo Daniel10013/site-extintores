eb5437e0fda3ba79a91131756c2c6817f0ff1b7f
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2024-05-16 12:03:42 -0300

    first commit

---
+++ routes.php
@@ -0,0 +1,10 @@
+<?php
+
+$routes = [
+    "" => "home",
+    "home" => "home",
+    "sobre-nos" => "about-us",
+    "produtos" => "products",
+    "produto" => "product-detail",
+    "contato" => "contact"
+];
