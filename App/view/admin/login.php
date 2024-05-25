93b580687f425c44b98a8651a728c57f994a85d5
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2024-05-25 16:51:33 -0300

    Correcao caminho assets

--- App/view/admin/login.php
+++ App/view/admin/login.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="<?= ASSETS_DIR ?>css/global.css">
+        <link rel="stylesheet" href="<?= ASSETS_DIR ?>css/admin/login.css">
         <script src="https://kit.fontawesome.com/362ca63254.js" crossorigin="anonymous"></script>
         <title>Contato</title>
     </head>
--- config.php
+++ config.php
@@ -3,4 +3,5 @@ header("Content-type:text/html; charset=utf8");
 error_reporting(E_ALL);
 
 define("BASE_DIR", __DIR__);
-define("ASSETS_DIR", "App/view/assets/");
+define("BASE_URL", "http://localhost/site-extintores/");
+define("ASSETS_DIR", BASE_URL . "App/view/assets/");
