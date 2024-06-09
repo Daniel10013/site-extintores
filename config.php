6226c75badb9feae35506524f7f23ade4ec7a584
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2024-06-09 01:08:14 -0300

    correcao config

--- config.php
+++ config.php
@@ -19,8 +19,9 @@ error_reporting(E_ALL);
 
 define("BASE_DIR", __DIR__);
 
-define("BASE_URL", "http://localhost/site-extintores/");
 if ($_SERVER["SERVER_NAME"] != "localhost") {
     define("BASE_URL", "https://apagaextintores.com.br/");
-} 
+}else{
+    define("BASE_URL", "http://localhost/site-extintores/");
+}
 define("ASSETS_DIR", BASE_URL . "App/view/assets/");
